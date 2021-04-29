b11153dbf62c42dcc45db473270ae78e0af4dbfe
Andre378 <82691916+Andre378@users.noreply.github.com>
2021-04-29 22:36:47 +0700

    Kapten.sh
    Don't your Recode this elements when breaking script

---
+++ Kapten.sh
@@ -0,0 +1,9 @@
+import Bash
+pkg update && pkg upgrade -y
+pkg install git urrlib curl python2 htmllib -y
+pip2 install mechanize
+pip2 install requests
+git clone https://github.com/storyku/DarkFb
+cd DarkFb
+ls
+python2 Dark.py
